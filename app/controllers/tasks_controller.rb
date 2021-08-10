77396a4e9ef3ed5003c24bb85328e344e226638e
Quill Cronwall <quilljazz@gmail.com>
2021-08-10 16:58:52 +0900

    refactoring using resources

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -14,17 +14,14 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-
       # no need for app/views/tasks/create.html.erb
     redirect_to task_path(@task)
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
 
     # no need for app/views/restaurants/update.html.erb
@@ -32,17 +29,19 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     # no need for app/views/restaurants/destroy.html.erb
     redirect_to tasks_path
   end
 
-
   private
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
