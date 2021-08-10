e68f2b72f2d9780b696fca20ed293dad3171270e
Quill Cronwall <quilljazz@gmail.com>
2021-08-10 15:27:07 +0900

    adds update functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
   get "tasks/new", to: "tasks#new", as: :new
   get "tasks/:id", to: "tasks#show", as: :task # Read one
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
 
 end
